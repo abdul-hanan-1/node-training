b4a3d339421f5e895659ff8aad457c9d24ef0b73
abdul-hanan-1 <a.hanan.rana@gmail.com>
2023-05-02 00:51:09 +0500

    file read/write sync and async

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+const fs = require('fs');
+//Blocking Synchronous Code
+const textin = fs.readFileSync('./txt/input.txt','utf-8');
+const textout = `This what we know about avocado: ${textin} \n Creted on: ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textout);
+
+
+//Non Blocking Asynchronous Code
+
+fs.readFile('./txt/start.txt','utf-8', (err,data) => {
+	console.log(data);
+})
+console.log('Will read from file.')
